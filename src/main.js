1c473f0f06cd3fd9408c1f639d62b7abc94ddff2
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-03-28 10:24:14 -0600

    chore: Install buefy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+
 import App from "./App.vue";
 import store from "./store";
 import "./registerServiceWorker";
 
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
