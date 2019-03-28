88c6a6946860e9b5de6c3ff196beb80be4d5a5a4
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-03-28 12:48:58 -0600

    chore: Install vue-resource

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
+import VueResource from "vue-resource";
 
 import App from "./App.vue";
 import store from "./store";
 import "./registerServiceWorker";
 
 Vue.use(Buefy);
+Vue.use(VueResource);
 Vue.config.productionTip = false;
 
 new Vue({
