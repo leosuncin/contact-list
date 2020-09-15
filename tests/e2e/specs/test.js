0c5a64c53ed02164a4ba8810351ee1f8bead57fa
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-09-15 08:36:48 -0600

    test(e2e): Fix tests to the new version of testing library
    Remove selectors chain.
    Prefer select by role over CSS selectors.
    Add "Triple Slash Directives" to detect typings

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+/// <reference types="@testing-library/cypress" />
 // https://docs.cypress.io/api/introduction/api.html
 const faker = require("faker");
 
@@ -12,69 +14,54 @@ describe("Contact list", () => {
   });
 
   it("Should show a table with the contacts", () => {
-    cy.get("tbody tr")
-      .its("length")
-      .should("be.gte", 9);
+    cy.findAllByRole("row").should("have.length.gte", 9);
   });
 
   it("Should show the details of one contact", () => {
-    cy.get(".chevron-cell:first > a").click();
-    cy.get("tr.detail")
-      .its("length")
-      .should("be.eq", 1);
+    cy.findAllByRole("row").eq(1).findByRole("button").click();
+    cy.findByRole("article").should("exist");
   });
 
   it("Should create one contact", () => {
     const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
 
-    cy.findByText(/Add contact/i)
-      .click()
-      .findByText(/Create contact/i)
-      .should("be.visible");
+    cy.findByRole("button", { name: /Add contact/i }).click();
+    cy.findByText(/Create contact/i).should("be.visible");
 
-    cy.findByText(/Basic/i)
-      .click()
-      .findByLabelText(/Full name/i)
-      .type(`${firstName} ${lastName}`)
-      .findByLabelText(/Username/i)
-      .type(faker.internet.userName(firstName, lastName))
-      .findByLabelText(/e-mail/i)
-      .type(faker.internet.email(firstName, lastName))
-      .findByLabelText(/Phone Number/i)
-      .type(faker.phone.phoneNumber("####-####"))
-      .findByLabelText(/website/i)
-      .type(faker.internet.url());
+    cy.findByText(/Basic/i).click();
+    cy.findByLabelText(/Full name/i).type(`${firstName} ${lastName}`);
+    cy.findByLabelText(/Username/i).type(
+      faker.internet.userName(firstName, lastName)
+    );
+    cy.findByLabelText(/e-mail/i).type(
+      faker.internet.email(firstName, lastName)
+    );
+    cy.findByLabelText(/Phone Number/i).type(
+      faker.phone.phoneNumber("####-####")
+    );
+    cy.findByLabelText(/website/i).type(faker.internet.url());
 
-    cy.findByText(/Address/i)
-      .click()
-      .findByLabelText(/Street/i)
-      .type(faker.address.streetAddress())
-      .findByLabelText(/Suite/i)
-      .type(faker.address.secondaryAddress())
-      .findByLabelText(/City/i)
-      .type(faker.address.city())
-      .findByLabelText(/Zip Code/i)
-      .type(faker.address.zipCode("######"))
-      .findByPlaceholderText(/latitude/i)
-      .type(faker.address.latitude())
-      .findByPlaceholderText(/longitude/i)
-      .type(faker.address.longitude());
+    cy.findByText(/Address/i).click();
+    cy.findByLabelText(/Street/i).type(faker.address.streetAddress());
+    cy.findByLabelText(/Suite/i).type(faker.address.secondaryAddress());
+    cy.findByLabelText(/City/i).type(faker.address.city());
+    cy.findByLabelText(/Zip Code/i).type(faker.address.zipCode("######"));
+    cy.findByPlaceholderText(/latitude/i).type(faker.address.latitude());
+    cy.findByPlaceholderText(/longitude/i).type(faker.address.longitude());
 
-    cy.findByText(/Company/i)
-      .click()
-      .findByLabelText(/^Name/i)
-      .type(faker.company.companyName())
-      .findByLabelText(/Catch Phrase/i)
-      .type(faker.company.catchPhrase())
-      .findByLabelText(/Business/i)
-      .type(faker.company.bs());
+    cy.findByText(/Company/i).click();
+    cy.findByLabelText(/^Name/i).type(faker.company.companyName());
+    cy.findByLabelText(/Catch Phrase/i).type(faker.company.catchPhrase());
+    cy.findByLabelText(/Business/i).type(faker.company.bs());
 
-    cy.findByText(/Create/i, { selector: "button" }).click();
+    cy.findByRole("button", { name: /Create/i }).click();
 
     cy.get(".pagination .pagination-next").click();
 
-    cy.findByText(RegExp(`${firstName} ${lastName}`, "i")).should("be.visible");
+    cy.findByRole("row", {
+      name: RegExp(`${firstName} ${lastName}`, "i"),
+    }).should("be.visible");
   });
 
   it("Should update one contact", () => {
@@ -83,48 +70,37 @@ describe("Contact list", () => {
     const zipCode = faker.address.zipCode("######");
     const business = faker.company.bs();
 
-    cy.get(".chevron-cell:first > a")
-      .click()
-      .findByText(/^Update/i)
-      .click()
-      .findByText(/Update \w+ info/)
-      .should("be.visible");
+    cy.findAllByRole("row").eq(2).findByRole("button").click();
+    cy.findByRole("button", { name: /^Update/i }).click();
+    cy.findByText(/Update \w+ info/).should("be.visible");
 
-    cy.findByText(/Basic/i)
-      .click()
-      .findByLabelText(/^phone/i)
+    cy.findByText(/Basic/i).click();
+    cy.findByLabelText(/^phone/i)
       .clear()
-      .type(phoneNumber)
-      .findByLabelText(/^website/i)
+      .type(phoneNumber);
+    cy.findByLabelText(/^website/i)
       .clear()
       .type(website);
 
-    cy.findByText(/Address/i)
-      .click()
-      .findByLabelText(/Zip Code/i)
+    cy.findByText(/Address/i).click();
+    cy.findByLabelText(/Zip Code/i)
       .clear()
       .type(zipCode);
 
-    cy.findByText(/Company/i)
-      .click()
-      .findByLabelText(/Business/i)
+    cy.findByText(/Company/i).click();
+    cy.findByLabelText(/Business/i)
       .clear()
       .type(business);
 
     cy.findByText(/^Edit/i).click();
 
-    cy.contains('[data-label="Phone number"]:first', phoneNumber)
-      .get("tr.detail")
-      .contains(website);
+    cy.findByRole("cell", { name: phoneNumber });
+    cy.findByRole("article").contains(website);
   });
 
   it("Should delete one contact", () => {
-    cy.get(".chevron-cell:first > a")
-      .click()
-      .findByText(/^Remove/)
-      .click()
-      .get("tbody tr")
-      .its("length")
-      .should("be.eq", 8);
+    cy.findAllByRole("row").eq(3).findByRole("button").click();
+    cy.findByRole("button", { name: /^Remove/ }).click();
+    cy.findAllByRole("row").should("have.length.gte", 8);
   });
 });
